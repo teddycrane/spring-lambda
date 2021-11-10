db654a443fb7bc9d1c16a350a718345eeb61dc25
Teddy Crane <teddy.crane@slalom.com>
2021-11-10 13:16:11 -0800

    fix app config

--- src/main/java/com/teddycrane/App.java
+++ src/main/java/com/teddycrane/App.java
@@ -6,7 +6,7 @@ package com.teddycrane;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
         System.out.println( "Hello World!" );
     }
